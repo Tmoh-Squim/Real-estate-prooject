246b814e11bb70a390ffdc1c85f53ee930ae3e42
squim <squim@localhost.localdomain>
2024-07-01 17:55:11 +0300

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import "./App.css"
+import "./App.css";
 import React, { useEffect } from "react";
 import HomePage from "./routes/homePage/homePage";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
@@ -16,89 +13,87 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.min.css";
 import store from "./redux/store";
 import { getUser, getUsers } from "./redux/user";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import socket from "./socket";
 import { getEstates } from "./redux/product";
 import { getConversations } from "./redux/category";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import Contact from "./routes/contact/Contact";
 import ProtectedRoute, { AgentRoute } from "./middlewares/ProtectedAuth";
+
 function App() {
-  const {user} = useSelector((state)=>state.user);
-  useEffect(()=>{
-   user?.user && socket.emit('addUser',(user?.user?._id))
-  },[user])
-  useEffect(()=>{
-    store.dispatch(getUser())
-    store.dispatch(getEstates())
-  },[store])
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    user?.user && socket.emit("addUser", user?.user?._id);
+  }, [user]);
+
+  useEffect(() => {
+    store.dispatch(getUser());
+    store.dispatch(getEstates());
+  }, [store]);
+
   const dispatch = useDispatch();
-  useEffect(()=>{
+
+  useEffect(() => {
     dispatch(getConversations(user?.user?._id));
-    dispatch((getUsers()));
-  },[user])
- 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children:[
-        {
-          path:"/",
-          element:<ProtectedRoute>
-            <HomePage/>
-          </ProtectedRoute>
-        },
-        {
-          path:"/estates",
-          element:<ProtectedRoute>
-            <ListPage/>
-          </ProtectedRoute>
-        },
-        {
-          path:"/estate/:id",
-          element:<SinglePage/>
-        },
-        {
-          path:"/profile",
-          element: <ProtectedRoute>
-          <ProfilePage />
-        </ProtectedRoute>
-        },
-        {
-          path:"/update-profile",
-          element: <ProtectedRoute>
-          <ProfileUpdatePage />
-        </ProtectedRoute>
-        },
-        {
-          path:"/login",
-          element:<Login/>
-        },
-        {
-          path:"/register",
-          element:<Register/>
-        },
-        {
-          path:"/contact-us",
-          element:<Contact/>
-        },
-        {
-          path:"/create-post",
-          element:
-            <AgentRoute>
-              <CreatePost />
-            </AgentRoute>
-        }
-      ]
-    }
-  ]);
+    dispatch(getUsers());
+  }, [user]);
 
   return (
     <>
       <ToastContainer />
-      <RouterProvider router={router}/>
-      </>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/estates"
+              element={
+                <ProtectedRoute>
+                  <ListPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/estate/:id" element={<SinglePage />} />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/update-profile"
+              element={
+                <ProtectedRoute>
+                  <ProfileUpdatePage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route
+              path="/create-post"
+              element={
+                <AgentRoute>
+                  <CreatePost />
+                </AgentRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
