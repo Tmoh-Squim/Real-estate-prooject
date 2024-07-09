2e2c7fa7683c27ab9d4463770fb4b0bc2861a7f2
squim <squim@localhost.localdomain>
2024-07-09 10:47:47 +0300

    updated

--- frontend/src/socket.tsx
+++ frontend/src/socket.tsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Replace this URL with your server's URL
-const SERVER_URL = "http://localhost://2022";
+const SERVER_URL = "https://real-estate-socket-ekdr.onrender.com";
 //https://real-estate-socket-ekdr.onrender.com
 const socket = io(SERVER_URL, {
   transports: ['websocket'], // Use WebSocket only
