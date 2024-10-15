b36e4e6654aab0197e89097efe3b74a479660ba7
Timoth Kibunja <122232330+Tmoh-Squim@users.noreply.github.com>
2024-10-15 09:21:54 +0000

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cors({
     origin:[
         "http://localhost:5173",
+        "http://localhost:3000",
         "https://squim-real-estate.vercel.app"
     ],
     credentials:true
